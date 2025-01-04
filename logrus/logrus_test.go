919383078a0ed6513d1a517fcdf08ab89c15c446
Alden <ygq13572549874@163.com>
2025-01-04 16:41:14 +0800

    Create logrus_test.go
    Signed-off-by: Alden <ygq13572549874@163.com>

---
+++ logrus/logrus_test.go
@@ -0,0 +1,28 @@
+package logrus
+import (
+    "testing"
+    "context"
+    "encoding/json"
+    "github.com/satori/go.uuid"
+)
+
+func Test_NewLogrusLogger(t *testing.T) {
+    var cnf *LogrusConfig = &LogrusConfig{
+        Level: "info",
+        Format: "json",
+        LogPath: "./logs",
+        LogFile: "test.log",
+        MaxSize: 1,
+        MaxBackups: 10,
+        MaxAge: 10,
+        Compress: true,
+        CtxKey: "trace_id",
+    }
+
+    l := NewLogrusLogger(cnf)
+    ctx := context.WithValue(context.Background(),"trace_id", uuid.NewV4().String())
+    conf,_ := json.Marshal(cnf)
+    for i := 1; i <= 10000; i++ {
+        l.Infof("info:%v",string(conf),ctx)
+    }
+}
