a791d302f019d6e5fb635e1cb9a497d5475fe8cd
Alden <ygq13572549874@163.com>
2025-01-04 16:37:49 +0800

    Create zap_test.go
    Signed-off-by: Alden <ygq13572549874@163.com>

---
+++ zap/zap_test.go
@@ -0,0 +1,30 @@
+package zap
+import (
+    "testing"
+    //"errors"
+    //"fmt"
+    "context"
+    "encoding/json"
+    "github.com/satori/go.uuid"
+)
+
+func Test_NewZapLogger(t *testing.T) {
+    var cnf *ZapConfig = &ZapConfig{
+        Level: "info",
+        Format: "json",
+        LogPath: "./logs",
+        LogFile: "test.log",
+        MaxSize: 1,
+        MaxBackups: 10,
+        MaxAge: 10,
+        Compress: true,
+        CtxKey: "trace_id",
+    }
+
+    l := NewZapLogger(cnf)
+    ctx := context.WithValue(context.Background(),"trace_id", uuid.NewV4().String())
+    conf,_ := json.Marshal(cnf)
+    for i := 1; i <= 10000; i++ {
+        l.Infof("info:%v",string(conf),ctx)
+    }
+}
